aca5ab6d558a04cbc6296e14acb9c6a2dd0f8592
sravani-vallepu07 <sravanivallepu07@gmail.com>
2026-03-01 18:42:46 +0530

    Fix ReferenceError: path is not defined for dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 const express = require("express");
-const path = require("path");
 const multer = require("multer");
 const axios = require("axios");
 const FormData = require("form-data");
